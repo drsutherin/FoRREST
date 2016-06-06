c36ba8e03d7aa589640e4bbae21579c4cd9d244d
Logan Rickert <c9.io@rickertcorp.com>
2016-06-06 19:39:24 +0000

    Added idamous class and test main

---
+++ idamous.py
@@ -0,0 +1,12 @@
+class Idamous:
+    
+    def __init__(self):
+        """
+            Inits the class
+        """
+        pass
+
+# If you call idamous.py, run this.
+if __name__ == '__main__':
+    print 'Creating idamous instance'
+    idamous = Idamous()
