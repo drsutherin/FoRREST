73e3bef5036aa2bef1ec9990cfc2083e0d2982a0
David <sutherin.3@wright.edu>
2016-06-14 20:26:54 +0000

    Removed unnecessary md5 and sh1 directories

--- plugins/sha1/sha1.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-def checksum(binary):
-    p = subprocess.Popen(["sha1sum", binary.get_file()], stdout=subprocess.PIPE)
-    output, err = p.communicate()
-    return "\nSHA1 Sum:\t%s" % output.split(" ")[0]
