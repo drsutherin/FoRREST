af6547054b1f02655a098609ea66f2b76c7d58a2
David <sutherin.3@wright.edu>
2016-06-15 19:59:59 +0000

    Added primary user interaction loop and help file

--- idamous.py
+++ idamous.py
@@ -2,110 +2,168 @@
 
 import subprocess
 import os
+import sys
 from importlib import import_module
 from plugins import *
 
 # from plugins.elf import elf
 
 class Idamous:
-    
-    def __init__(self):
-        """
-            Inits the class
-        """
-        # Change to not being hardcoded later
-        self.operating_system = 'linux'
-        self.current_file = None
-        self.result_directory = None
-    
-    def set_file(self, filename):
-        self.current_file = filename
-    
-    def get_file(self):
-        return self.current_file
+	
+	def __init__(self):
+		"""
+			Inits the class
+		"""
+		# Change to not being hardcoded later
+		self.operating_system = 'linux'
+		self.current_file = None
+		self.result_directory = None
+	
+	def set_file(self, filename):
+		self.current_file = filename
+	
+	def get_file(self):
+		return self.current_file
+	
+	"""
+	Provides interaction to select a file to load
+	@param cmd: a list of strings in the form ['load', 'filename']
+	"""
+	def load_file(self, cmd):
+                try:
+			binary = cmd[1]
+                        f = open(binary)
+                        f.close()
+                        # Creates a directory for the results of the Idamous tests
+			idamous.set_file(binary)
+		        if "." in binary:
+         	        	splitFile = binary.split(".")[-2]
+	                	splitFile = splitFile.split("/")[-1]
+			else:
+				splitFile = binary.split("/")[-1]
 
-    def set_result_directory(self, dir):
-        self.result_directory = dir
+		        self.set_result_directory("./results/" + splitFile)
+		        # This could be dangerous...
+        		subprocess.Popen(["rm", "-rf", self.get_result_directory()])
 
-    def get_result_directory(self):
-        return self.result_directory
+		        subprocess.Popen(["mkdir", self.get_result_directory()])
+			print "File %s successfully loaded" % binary
 
-    def _call_shell_function(self, commandName, args):
-        """
-            Make a call to the terminal and return the output.
-        """
-        output = subprocess.check_output([commandName].extend(args))
-        return output
+                except IOError:
+                        print "File %s does not exist. Try again" % binary
 
-    def basics(self):
-        result = elf.elf.read_header(self)
-        result += md5.md5.checksum(self)
-        result += sha1.sha1.checksum(self)
-        return result
+	def set_result_directory(self, dir):
+		self.result_directory = dir
+
+	def get_result_directory(self):
+		return self.result_directory
+
+	def _call_shell_function(self, commandName, args):
+		"""
+			Make a call to the terminal and return the output.
+		"""
+		output = subprocess.check_output([commandName].extend(args))
+		return output
+
+	def basics(self):
+		result = elf.elf.read_header(self)
+		result += md5.md5.checksum(self)
+		result += sha1.sha1.checksum(self)
+		return result
+
+	"""
+	Get raw (level 0) data about the file.
+	@param cmd: 	A list of strings in the form ["get_raw", "-etc"] where e, t, and c are placeholders for
+			functionality we can add later, i.e. "-n" will only return the filename
+	TODO:		Examine the rest of the list to determine (and then implement) options as described above
+	"""
+	def get_raw_data(self, cmd):
+		 # Get raw data
+	        raw_data = raw.File.File(self.get_file())
+
+        	print 'Filename:', raw_data.get_name()
+	        print 'File extension:', raw_data.get_extension()
+		print 'File size:', raw_data.get_size()
+	        print 'File inode:', raw_data.get_inode()
+        	print 'File path:', raw_data.get_path()
+	        print 'File MD5:', raw_data.get_md5()
+	        print 'File SHA1:', raw_data.get_sha1()
+
+	        raw_file = self.get_result_directory() + "/raw_data.txt"
+
+	        # Open file to write raw data, create one if necessary
+	        if not os.path.isfile(raw_file):
+        	        subprocess.Popen(["touch", raw_file])
+	        f = open(raw_file, "w")
+
+	        f.write("Filename: %s\n" % raw_data.get_name())
+	        f.write('File extension: %s\n' % raw_data.get_extension())
+	        f.write('File size: %s\n' % raw_data.get_size())
+	        f.write('File inode: %s\n' % raw_data.get_inode())
+	        f.write('File path: %s\n' % raw_data.get_path())
+	        f.write('File MD5: %s\n' % raw_data.get_md5())
+	        f.write('File SHA1: %s\n' % raw_data.get_sha1())
+        	f.close()
+
+	        print "Raw data saved to %s" % raw_file
+
+	"""
+	Get extracted (level 1) data about the file.
+        @param cmd:     A list of strings in the form ["get_extracted", "-etc"] where e, t, and c are placeholders for
+                        functionality we can add later
+        TODO:           Examine the rest of the list to determine (and then implement) options as described above
+	"""
+	def get_extracted_data(self, cmd):
+		# Get extracted data and write to file
+	        extracted_data = extract.Extract.Extract(idamous.get_file())
+
+        	extracted_file = idamous.get_result_directory() + "/extracted_data.txt"
+	        if not os.path.isfile(extracted_file):
+        	        subprocess.Popen(["touch", extracted_file])
+	        e = open(extracted_file, "w")
+
+	        print 'Filetype:', extracted_data.get_filetype()
+	        e.write('Filetype: %s\n' % extracted_data.get_filetype())
+
+	     	print "Extracted data saved to %s" % extracted_file
+	        e.close()
+
+
 
 # If you call idamous.py, run this.
 if __name__ == '__main__':
-    print 'Creating idamous instance'
-    idamous = Idamous()
-    
-    # Allow user to specify file to test
-    while True:
-        try:
-            print 'Enter name of binary file: '
-            binary = raw_input()
-            f = open(binary)
-            f.close()
-            break
-        except IOError:
-            print "File %s does not exist. Try again" % binary
-    
-    # Creates a directory for the results of the Idamous tests
-    idamous.set_file(binary)
-    if "." in binary:
-        splitFile = binary.split(".")[-2]
-        splitFile = splitFile.split("/")[-1]
+	print 'Creating idamous instance'
+	idamous = Idamous()
+	
+	# Primary control loop
+	print "IDAMOUS: Integrated Framework for Reverse Engineering Software"
+	while True:
+		sys.stdout.flush()
+		sys.stdout.write("idamous->")
+		sys.stdout.flush()
+		cmd = raw_input()
+		cmd = cmd.split(" ")
+		
+		print "cmd[0] is: %s" % cmd[0]
+		if idamous.get_file() == None and not (cmd[0] == "load" or cmd[0] == "exit" or cmd[0] == "help"):
+			print "No file loaded. Use 'load' command"
 
-    # Assumes it will receive a file with an extension    
-    idamous.set_result_directory("./results/" + splitFile)
-    # This could be dangerous...
-    subprocess.Popen(["rm", "-rf", idamous.get_result_directory()])
-    subprocess.Popen(["mkdir", idamous.get_result_directory()])
+		elif cmd[0] == "get_extracted":
+			idamous.get_extracted_data(cmd)
 
-    # Get raw data
-    raw_data = raw.File.File(idamous.get_file())
-    
-    print 'Filename:', raw_data.get_name()
-    print 'File extension:', raw_data.get_extension()
-    print 'File size:', raw_data.get_size()
-    print 'File inode:', raw_data.get_inode()
-    print 'File path:', raw_data.get_path()
-    print 'File MD5:', raw_data.get_md5()
-    print 'File SHA1:', raw_data.get_sha1()
-    
-    raw_file = idamous.get_result_directory() + "/raw_data.txt"
+		elif cmd[0] == "get_raw":
+			idamous.get_raw_data(cmd)
 
-    # Open file to write raw data, create one if necessary
-    if not os.path.isfile(raw_file):
-        subprocess.Popen(["touch", raw_file])
-    f = open(raw_file, "w")
-    
-    f.write("Filename: %s\n" % raw_data.get_name())
-    f.write('File extension: %s\n' % raw_data.get_extension())
-    f.write('File size: %s\n' % raw_data.get_size())
-    f.write('File inode: %s\n' % raw_data.get_inode())
-    f.write('File path: %s\n' % raw_data.get_path())
-    f.write('File MD5: %s\n' % raw_data.get_md5())
-    f.write('File SHA1: %s\n' % raw_data.get_sha1())
-    f.close()
+		elif cmd[0] == "help":
+			f = open("help.txt")
+			for line in f:
+				print line
+			f.close()
 
-    # Get extracted data
-    extracted_data = extract.Extract.Extract(idamous.get_file())
-    
-    extracted_file = idamous.get_result_directory() + "/extracted_data.txt"
-    if not os.path.isfile(extracted_file):
-        subprocess.Popen(["touch", extracted_file])
-    e = open(extracted_file, "w")
+		elif cmd[0] == "load":
+			idamous.load_file(cmd)
 
-    print 'Filetype:', extracted_data.get_filetype()
-    e.write('Filetype: %s\n' % extracted_data.get_filetype())
-    e.close()
+		elif cmd[0] == "quit" or cmd[0] == "exit":
+			break;
+		else:
+			print "Command not recognized. Try again or type 'help'."
