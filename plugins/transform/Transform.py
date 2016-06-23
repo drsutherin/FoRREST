0666c5eb3a6d5a8720a4f486cfc5882b6a1fceef
DSUTHERIN <drsutherin@github.com>
2016-06-23 10:57:27 -0400

    Added get_strings function and test binaries from /usr/bin

--- plugins/transform/Transform.py
+++ plugins/transform/Transform.py
@@ -1,27 +1,27 @@
 
 class Transformed:
-    
-    def __init__(self, idamous):
-        self.idamous = idamous
-    
-    def _get_file(self):
-        return self.idamous.get_file()
-        
-    def get_disassembly(self):
-        pass
-    
-    def get_mnemonics(self):
-        pass
-    
-    def get_functions(self):
-        pass
-    
-    def get_basic_blocks(self):
-        pass
+	
+	def __init__(self, idamous):
+		self.idamous = idamous
+	
+	def _get_file(self):
+		return self.idamous.get_file()
+		
+	def get_disassembly(self):
+		pass
+	
+	def get_mnemonics(self):
+		pass
+	
+	def get_functions(self):
+		pass
+	
+	def get_basic_blocks(self):
+		pass
 
-    def get_data_references(self):
-        pass
-    
-    def get_jump_targets(self):
-        pass
-    
+	def get_data_references(self):
+		pass
+	
+	def get_jump_targets(self):
+		pass
+	
