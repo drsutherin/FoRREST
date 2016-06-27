b92ea5bee0b5582f2723273598552fb2e5407fc0
Logan Rickert <c9.io@rickertcorp.com>
2016-06-27 00:55:27 +0000

    Changed html/* to /*

--- renameFiles.bash
+++ renameFiles.bash
@@ -3,8 +3,8 @@
 htmldir=html
 stringtofind1="_static"
 stringtofind2="_sources"
-stringreplace1="html/static"
-stringreplace2="html/sources"
+stringreplace1="static"
+stringreplace2="sources"
 
 cd $htmldir
 
