61ff889c4c82cf5f54ce3a9daad4353367d87831
drsutherin <sutherin.3@wright.edu>
2016-06-29 10:47:22 -0400

    Revert "Updated documentation and standard."
    This reverts commit e57b4531943b0a7572ffec64352b0a37f7a6f0f8.

--- Idamous/plugins/raw/File.py
+++ Idamous/plugins/raw/File.py
@@ -1,234 +1,132 @@
 
-"""
-    :Platform: Unix
-    :Class Authors: Authors: Logan Rickert, Cole Loewer, David Sutherin
-    :Standard: Flake8
-
-    The main purpose of this class is to handle getting meta-data about files.
-"""
-
 import os
 import hashlib
 
-
 class File:
-
+    
     def __init__(self, idamous):
-        """
-            Description:
-                This sets up the class.
-
-            Args:
-                idamous: The idamous instance to work with.
-
-            Returns:
-                None.
-        """
         self.idamous = idamous
         self.file = None
-
+        
     def _get_file(self):
-        """
-            Description:
-                This function gets the filepath and name of the file.
-
-            Returns:
-                str. Filepath and name.
-        """
         return self.idamous.get_file()
-
+        
     def get_name(self):
         """
-            Description:
-                Returns the filename of the current file.
-
-            Example:
+            Returns the filename of the current file.
+            
+            Example: 
                 file = /cake/awesome/sweet.py
-                returns: "sweet.py"
-
-            Returns:
-                str. The filename.
+                returns: "sweet.py" 
         """
         return os.path.basename(self._get_file())
-
+        
     def get_extension(self):
         """
-            Description:
-                Returns the extension of the current file.
-
+            Returns the extension of the current file.
+            
             Example:
                 file = /cake/awesome/sweet.py
                 returns: "py"
-
-            Returns:
-                str. The extenstion.
         """
         temp = self._get_file().split('.')
         ext = ""
-
+        
         # Check to see if there is a dot.
         if len(temp) > 1:
             ext = temp[-1]
-
+        
         return ext
 
     def get_size(self):
         """
-            Description:
-                Returns the size of the current file in bytes.
-
+            Returns the size of the current file in bytes.
+            
             Example:
                 3343 idamous.py
                 returns: 3343
-
-            Returns:
-                int. The size of the file.
         """
         return os.stat(self._get_file()).st_size
-
+        
     def get_inode(self):
         """
-            Description:
-                Returns the inode of the current file.
-
+            Returns the inode of the current file.
+            
             Example:
                 303 idamous.py
                 returns: 303
-
-            Returns:
-                int. The inode.
         """
         return os.stat(self._get_file()).st_ino
-
+    
     def get_path(self):
         """
-            Description:
-                Returns the path to the file.
-
+            Returns the path to the file.
+            
             Example:
                 file = /cake/awesome/sweet.py
                 returns: "/cake/awesome"
-
+                
                 file = plugins/raw/Raw.py
                 returns: "plugins/raw"
-
-            Returns:
-                str. The path.
         """
         return os.path.dirname(self._get_file())
-
+        
     def get_md5(self):
         """
-            Description:
-                Returns the MD5 sum for the file.
-
+            Returns the MD5 sum for the file.
+            
             Example:
                 file = /cake/awesome/sweet.py
                 returns: "943172131622f261d9af95e1634159d9"
-
-            Returns:
-                str. The hash.
         """
         hash_algo = hashlib.md5()
         return File.hash_file(self._get_file(), hash_algo)
-
+        
     def get_sha1(self):
-        """
-            Description:
-                Returns the SHA1 sum for the file.
-
-            Example:
-                file = /cake/awesome/sweet.py
-                returns: "943172131622f261d9af95e1634159d9"
-
-            Returns:
-                str. The hash.
-        """
         hash_algo = hashlib.sha1()
         return File.hash_file(self._get_file(), hash_algo)
-
+        
     def get_sha256(self):
-        """
-            Description:
-                Returns the SHA256 sum for the file.
-
-            Example:
-                file = /cake/awesome/sweet.py
-                returns: "943172131622f261d9af95e1634159d9"
-
-            Returns:
-                str. The hash.
-        """
         hash_algo = hashlib.sha256()
         return File.hash_file(self._get_file(), hash_algo)
-
+    
     def get_sha512(self):
-        """
-            Description:
-                Returns the SHA512 sum for the file.
-
-            Example:
-                file = /cake/awesome/sweet.py
-                returns: "943172131622f261d9af95e1634159d9"
-
-            Returns:
-                str. The hash.
-        """
         hash_algo = hashlib.sha512()
         return File.hash_file(self._get_file(), hash_algo)
 
     def read_chunck(self, chunksize=4096):
         """
-            Description:
-                Returns a chunck of the file. By default, the size is 4096.
-                The function will start at the beginning and hold its position
-                each read.
-                Once the file has been completely read in, calling this
-                function will return empty string ('') and reset to the
-                beginning of the file.
-
-            Args:
-                chunksize (int): The lenght of string to read in.
-
-            Returns:
-                str. The chunck from the requested file in rb.
+            Returns a chunck of the file. By default, the size is 4096.
+            The function will start at the beginning and hold its position
+            each read.
+            Once the file has been completely read in, calling this function
+            will return empty string ('') and reset to the beginning of the
+            file.
         """
-        if self.file is None:
+        if self.file == None:
             self.file = open(self._get_file(), 'rb')
 
         chunk = self.file.read(chunksize)
         if not chunk:
             self.reset_read()
-
+            
         return chunk
-
+        
     def reset_read(self):
         """
-            Description:
-                Force reset the read_chunck method to start at the beginning.
-
-            Returns:
-                None.
+            Force reset the read_chunck method to start at the beginning.
         """
-        if self.file is not None:
+        if self.file != None:
             self.file.close()
             self.file = None
 
     @staticmethod
     def hash_file(filename, hash_algo):
         """
-            Description:
-                Returns a hash for @filename based on the the @hash_algo.
-
-            Args:
-                filename (str): The name of the file to hash.
-                hash_algo (hashlib): The hashing algorithm.
-                (IE: hashlib.sha512())
-
-            Returns:
-                str. The hash.
+            Returns a hash for @filename based on the the @hash_algo.
         """
         with open(filename, "rb") as f:
             for chunk in iter(lambda: f.read(4096), b""):
                 hash_algo.update(chunk)
         return hash_algo.hexdigest()
+
--- Idamous/plugins/transform/Transform.py
+++ Idamous/plugins/transform/Transform.py
@@ -1,160 +1,70 @@
-"""
-    :Platform: Unix
-    :Class Authors: Authors: Logan Rickert, Cole Loewer, David Sutherin
-    :Standard: Flake8
-
-    The main purpose of this class is to transform the interpred data.
-
-    Requirements:
-        * radare2
-        * r2pipe
-"""
-
-
+#requires radare2 to be installed
 class Transform:
-
+    
     def __init__(self, idamous):
-        """
-            Description:
-                This sets up the class.
-
-            Args:
-                `idamous` (Idamous): The idamous instance to work with.
-
-            Returns:
-                None
-        """
         self.idamous = idamous
+    
+    # def _get_file(self):
+    #     return self.idamous.get_file()
 
-    def _get_file(self):
-        """
-            Description:
-                This function gets the filepath and name of the file.
+    # def check_imports(self):
+    #     good = True
+    #     try:
+    #         import r2pipe
+    #     except ImportError:
+    #         print "[-] Failed to load r2pipe"
+    #         print "[-] Do you have it installed?"
+    #         good = False
+    #     return good
 
-            Returns:
-                str - Filepath and name
-        """
-        return self.idamous.get_file()
+    # def get_disassembly(self):
+    #     if self.check_import() == True:
+    #         r2 = r2pipe.open(self._get_file)
+    #         out = r2.cmd('pd $s ~!invalid')
+    #         #need to figure out how to parse this
+    #         #there is a function to turn the information to json format built into radare2
+    #     return out
 
-    def check_imports(self):
-        """
-            Description:
-                Checks to make sure all of the required imports
-                have been loaded.
+    # def get_mnemonics(self):
+    #     if self.check_imports() == True:
+    #         r2 = r2pipe.open(self._get_file)
+    #         out = r2.cmd('pi $s ~!invalid')
+    #     return out
 
-            Returns:
-                bool - if import was sucessful.
-        """
-        good = True
-        try:
-            import r2pipe
-        except ImportError:
-            print "[-] Failed to load r2pipe"
-            print "[-] Do you have it installed?"
-            good = False
-        return good
+    # def get_functions(self):
+    #     #needs parsing
+    #     if self.check_imports() == True:
+    #         r2 = r2pipe.open(self._get_file)
+    #         r2.cmd('af')
+    #         out = r2.cmd('afl')
+    #     return out
 
-    def get_disassembly(self):
-        """
-            Description:
-                Gets the disassembly for a given file.
+    # def get_basic_blocks(self):
+    #     '''
+    #         if self.check_imports() == True:
+    #         r2 = r2pipe.open(self._get_file)
+    #         r2.cmd('af')
+    #         r2.cmd('ag > b.dot')
+    #         #displays with xdot not really what i want
+    #         out = r2.cmd('!xdot b.dot')
+    #         #not done
+    #     return out
+    #     '''
+    #     pass
 
-            Returns:
-                None
-        """
-        if self.check_imports():
-            r2 = r2pipe.open(self._get_file)
-            out = r2.cmd('pd $s ~!invalid')
-            # need to figure out how to parse this
-            # there is a function to turn the information to json format built
-            # into radare2
-            r2.quit()
-        return out
-
-    def get_mnemonics(self):
-        """
-            Description:
-                Gets the portion of the assembly instruction that is related to
-                the action being preformed.
-
-            Returns:
-                None
-        """
-        if self.check_imports():
-            r2 = r2pipe.open(self._get_file)
-            out = r2.cmd('pi $s ~!invalid')
-            r2.quit()
-        return out
-
-    def get_functions(self):
-        """
-            Description:
-                Gets the functions used in the program.
-
-            Returns:
-                None
-        """
-        #needs parsing
-        if self.check_imports():
-            r2 = r2pipe.open(self._get_file)
-            r2.cmd('af')
-            out = r2.cmd('afl')
-            r2.quit()
-        return out
-
-    def get_basic_blocks(self):
-        """
-            Description:
-                Gets the directed graph of basic blocks that can be
-                constructed by analyzing the boundaries and control
-                flow of instructions.
-
-            Returns:
-                None
-        """
-
-        '''
-            if self.check_imports() == True:
-            r2 = r2pipe.open(self._get_file)
-            r2.cmd('af')
-            r2.cmd('ag > b.dot')
-            #displays with xdot not really what i want
-            out = r2.cmd('!xdot b.dot')
-            #not done
-        return out
-        '''
-        pass
-
-    def get_data_references(self):
-        """
-            Description:
-                The location of data values derived from interpreting memory
-                store and load operations.
-
-            Returns:
-                None
-        """
-        '''
-        if self.check_imports() == True:
-            r2 = r2pipe.open(self._get_file)
-            r2.cmd('/R ')
-        return out
-        '''
-        pass
-
-    def get_jump_targets(self):
-        """
-            Description:
-                The memory address offsets obtained by analyzing the targets
-                of conditional and unconditional control flow change
-                instructions.
-
-            Returns:
-                None
-        """
-        #needs parsing
-        if self.check_imports() is True:
-            r2 = r2pipe.open(self._get_file)
-            r2.cmd('/R j ~j')
-            r2.close()
-            pass
+    # def get_data_references(self):
+    #     '''
+    #     if self.check_imports() == True:
+    #         r2 = r2pipe.open(self._get_file)
+    #         r2.cmd('/R ')
+    #     return out
+    #     '''
+    #     pass
+    
+    # def get_jump_targets(self):
+    #     #needs parsing
+    #     if self.check_imports() == True:
+    #         r2 = r2pipe.open(self._get_file)
+    #         r2.cmd('/R j ~j')
+    #         pass
+    
