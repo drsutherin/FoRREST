158e348ffe9113fa0c81849062304fcda8370151
drsutherin <sutherin.3@wright.edu>
2016-07-22 16:02:05 -0400

    get_cfg now displays CFG, added crackmes

--- FoRREST/plugins/infer/Infer.py
+++ FoRREST/plugins/infer/Infer.py
@@ -8,13 +8,22 @@ class Infer:
     def get_cfg(self):
         try:
 	    import angr
-            #from angrutils import *
+            # Create CFG
             from .visualize import plot_cfg # this is the only thing necessary from angrutils
             proj = angr.Project(self._get_file(), load_options={'auto_load_libs':False})
             main = proj.loader.main_bin.get_symbol("main")
             start_state = proj.factory.blank_state(addr=main.addr)
             cfg = proj.analyses.CFGAccurate(fail_fast=True, starts=[main.addr], initial_state=start_state)
             plot_cfg(cfg, "%s_cfg" % self._get_file(), asminst=True, remove_imports=True, remove_path_terminator=True)
+	    # Find and display CFG
+	    import subprocess
+	    fileToSend = self._get_file() + "_cfg.png"
+	    print fileToSend
+            p = subprocess.Popen(["locate", fileToSend], stdout = subprocess.PIPE)
+	    foundFile = p.stdout.readline().split('\n')[0]
+	    print foundFile
+            if foundFile is not "":
+                subprocess.Popen(["xdg-open", foundFile])
             return "[+] CFG saved as %s_cfg.png" % self._get_file()
         except ImportError:
             print "[-] Could not load angr"
